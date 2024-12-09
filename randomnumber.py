b35e805ac2c2a0199e30ef60d39ae229754874c8
Ghoul <CursedGhoulBlox@gmail.com>
2024-12-08 21:18:59 -0500

    Update randomnumber.py

--- randomnumber.py
+++ randomnumber.py
@@ -1,4 +1,7 @@
 import random
 
-randomNumber = random.randint(0,0) # Change the first number to set the minimum number, and the second number to change the maximum number
+minimum_number = int(input("Enter the minimum number that you want to be generated(Recommended:0): "))
+maximum_number = int(input("Enter the maximum number you want generated: "))
+
+randomNumber = random.randint(minimum_number,maximum_number) # Change the first number to set the minimum number, and the second number to change the maximum number
 print(randomNumber)
