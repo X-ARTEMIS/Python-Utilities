e606891f633423a2228ef60e56549bd7fb7b43c4
Ghoul <CursedGhoulBlox@gmail.com>
2025-01-29 17:16:11 -0500

    Delete Programs/MathPrograms directory

--- Programs/MathPrograms/TimesTables.py
+++
@@ -1,7 +0,0 @@
-num=int(input("Enter the number for the times table: ")) # Generate times tables for numbers quickly!
-
-mylist=range(1,11)
-
-for x in mylist:
-    ans=x*num
-    print(num, "x",x,"=", ans)
